f5996de019356acb359dd7d80781e546b646afd2
linD026 <0086d026@email.ntou.edu.tw>
2021-05-02 15:32:37 +0800

    add return type for setting the function type

--- template.h
+++ template.h
@@ -11,16 +11,18 @@
 #define VA_REMAIN_1(not, ...) VA_REMAIN_0(__VA_ARGS__)
 #define VA_REMAIN_0(not, ...) __VA_ARGS__
 
-#define t_node(name, type, parameter) t_node_(name, type, parameter)
-#define t_node_(name, type, parameter)                                         \
+#define t_node(return_type, name, type, parameter)                             \
+  t_node_(return_type, name, type, parameter)
+#define t_node_(return_type, name, type, parameter)                            \
   struct __t_node_##name_##type {                                              \
     type __dummy;                                                              \
-    void (*func)(type * arg, parameter);                                       \
+    return_type (*func)(type * arg, parameter);                                \
   }
 
-#define t_node_func(type, name, parameter) t_node_func_(type, name, parameter)
-#define t_node_func_(type, name, parameter)                                    \
-  void t_##name_##type(type *arg, parameter)
+#define t_node_func(return_type, type, name, parameter)                        \
+  t_node_func_(return_type, type, name, parameter)
+#define t_node_func_(return_type, type, name, parameter)                       \
+  return_type t_##name_##type(type *arg, parameter)
 
 #define t_node_declaration(type) t_node_declaration_(type)
 #define t_node_declaration_(type0) type_##type0
@@ -28,60 +30,60 @@
 #define t_node_func_declaration(name, type) t_node_func_declaration_(name, type)
 #define t_node_func_declaration_(name, type) t_##name_##type
 
-#define template_function(type, name, parameter, ...)                          \
-  template_function_(type, name, parameter, __VA_ARGS__)
-#define template_function_(type, name, parameter, ...)                         \
-  void t_##name_##type(type *arg, parameter) {                                 \
+#define template_function(return_type, type, name, parameter, ...)             \
+  template_function_(return_type, type, name, parameter, __VA_ARGS__)
+#define template_function_(return_type, type, name, parameter, ...)            \
+  return_type t_##name_##type(type *arg, parameter) {                          \
     typedef type T;                                                            \
     { __VA_ARGS__ }                                                            \
   }
 
-#define template_list(name, parameter, ...)                                    \
-  t_node_func(VA_EXTRACT_0(__VA_ARGS__), name, parameter);                     \
-  t_node_func(VA_EXTRACT_1(__VA_ARGS__), name, parameter);                     \
+#define template_list(return_type, name, parameter, ...)                       \
+  t_node_func(return_type, VA_EXTRACT_0(__VA_ARGS__), name, parameter);        \
+  t_node_func(return_type, VA_EXTRACT_1(__VA_ARGS__), name, parameter);        \
   struct __template_list_##name {                                              \
-    t_node(name, VA_EXTRACT_0(__VA_ARGS__), parameter)                         \
+    t_node(return_type, name, VA_EXTRACT_0(__VA_ARGS__), parameter)            \
         t_node_declaration(VA_EXTRACT_0(__VA_ARGS__));                         \
-    t_node(name, VA_EXTRACT_1(__VA_ARGS__), parameter)                         \
+    t_node(return_type, name, VA_EXTRACT_1(__VA_ARGS__), parameter)            \
         t_node_declaration(VA_EXTRACT_1(__VA_ARGS__));                         \
   } type_list_##name = {                                                       \
       .t_node_declaration(VA_EXTRACT_0(__VA_ARGS__)).func =                    \
           t_node_func_declaration(name, VA_EXTRACT_0(__VA_ARGS__)),            \
       .t_node_declaration(VA_EXTRACT_1(__VA_ARGS__)).func =                    \
           t_node_func_declaration(name, VA_EXTRACT_1(__VA_ARGS__))};           \
-  template_function(VA_EXTRACT_0(__VA_ARGS__), name, parameter,                \
+  template_function(return_type, VA_EXTRACT_0(__VA_ARGS__), name, parameter,   \
                     VA_REMAIN_1(__VA_ARGS__))                                  \
-      template_function(VA_EXTRACT_1(__VA_ARGS__), name, parameter,            \
-                        VA_REMAIN_1(__VA_ARGS__))
+      template_function(return_type, VA_EXTRACT_1(__VA_ARGS__), name,          \
+                        parameter, VA_REMAIN_1(__VA_ARGS__))
 
-#define template_list2(name, parameter, ...)                                   \
-  t_node_func(VA_EXTRACT_0(__VA_ARGS__), name, parameter);                     \
-  t_node_func(VA_EXTRACT_1(__VA_ARGS__), name, parameter);                     \
+#define template_list2(return_type, name, parameter, ...)                      \
+  t_node_func(return_type, VA_EXTRACT_0(__VA_ARGS__), name, parameter);        \
+  t_node_func(return_type, VA_EXTRACT_1(__VA_ARGS__), name, parameter);        \
   struct __template_list_##name {                                              \
-    t_node(name, VA_EXTRACT_0(__VA_ARGS__), parameter)                         \
+    t_node(return_type, name, VA_EXTRACT_0(__VA_ARGS__), parameter)            \
         t_node_declaration(VA_EXTRACT_0(__VA_ARGS__));                         \
-    t_node(name, VA_EXTRACT_1(__VA_ARGS__), parameter)                         \
+    t_node(return_type, name, VA_EXTRACT_1(__VA_ARGS__), parameter)            \
         t_node_declaration(VA_EXTRACT_1(__VA_ARGS__));                         \
   } type_list_##name = {                                                       \
       .t_node_declaration(VA_EXTRACT_0(__VA_ARGS__)).func =                    \
           t_node_func_declaration(name, VA_EXTRACT_0(__VA_ARGS__)),            \
       .t_node_declaration(VA_EXTRACT_1(__VA_ARGS__)).func =                    \
           t_node_func_declaration(name, VA_EXTRACT_1(__VA_ARGS__))};           \
-  template_function(VA_EXTRACT_0(__VA_ARGS__), name, parameter,                \
+  template_function(return_type, VA_EXTRACT_0(__VA_ARGS__), name, parameter,   \
                     VA_REMAIN_1(__VA_ARGS__))                                  \
-      template_function(VA_EXTRACT_1(__VA_ARGS__), name, parameter,            \
-                        VA_REMAIN_1(__VA_ARGS__))
+      template_function(return_type, VA_EXTRACT_1(__VA_ARGS__), name,          \
+                        parameter, VA_REMAIN_1(__VA_ARGS__))
 
-#define template_list3(name, parameter, ...)                                   \
-  t_node_func(VA_EXTRACT_0(__VA_ARGS__), name, parameter);                     \
-  t_node_func(VA_EXTRACT_1(__VA_ARGS__), name, parameter);                     \
-  t_node_func(VA_EXTRACT_2(__VA_ARGS__), name, parameter);                     \
+#define template_list3(return_type, name, parameter, ...)                      \
+  t_node_func(return_type, VA_EXTRACT_0(__VA_ARGS__), name, parameter);        \
+  t_node_func(return_type, VA_EXTRACT_1(__VA_ARGS__), name, parameter);        \
+  t_node_func(return_type, VA_EXTRACT_2(__VA_ARGS__), name, parameter);        \
   struct __template_list_##name {                                              \
-    t_node(name, VA_EXTRACT_0(__VA_ARGS__), parameter)                         \
+    t_node(return_type, name, VA_EXTRACT_0(__VA_ARGS__), parameter)            \
         t_node_declaration(VA_EXTRACT_0(__VA_ARGS__));                         \
-    t_node(name, VA_EXTRACT_1(__VA_ARGS__), parameter)                         \
+    t_node(return_type, name, VA_EXTRACT_1(__VA_ARGS__), parameter)            \
         t_node_declaration(VA_EXTRACT_1(__VA_ARGS__));                         \
-    t_node(name, VA_EXTRACT_2(__VA_ARGS__), parameter)                         \
+    t_node(return_type, name, VA_EXTRACT_2(__VA_ARGS__), parameter)            \
         t_node_declaration(VA_EXTRACT_2(__VA_ARGS__));                         \
   } type_list_##name = {                                                       \
       .t_node_declaration(VA_EXTRACT_0(__VA_ARGS__)).func =                    \
@@ -91,12 +93,12 @@
       .t_node_declaration(VA_EXTRACT_2(__VA_ARGS__)).func =                    \
           t_node_func_declaration(name, VA_EXTRACT_2(__VA_ARGS__)),            \
   };                                                                           \
-  template_function(VA_EXTRACT_0(__VA_ARGS__), name, parameter,                \
+  template_function(return_type, VA_EXTRACT_0(__VA_ARGS__), name, parameter,   \
                     VA_REMAIN_2(__VA_ARGS__))                                  \
-      template_function(VA_EXTRACT_1(__VA_ARGS__), name, parameter,            \
-                        VA_REMAIN_2(__VA_ARGS__))                              \
-          template_function(VA_EXTRACT_2(__VA_ARGS__), name, parameter,        \
-                            VA_REMAIN_2(__VA_ARGS__))
+      template_function(return_type, VA_EXTRACT_1(__VA_ARGS__), name,          \
+                        parameter, VA_REMAIN_2(__VA_ARGS__))                   \
+          template_function(return_type, VA_EXTRACT_2(__VA_ARGS__), name,      \
+                            parameter, VA_REMAIN_2(__VA_ARGS__))
 
 #define template_call(type, name, arg, parameter)                              \
   type_list_##name.type_##type.func(&arg, parameter);
--- test_struct.c
+++ test_struct.c
@@ -23,15 +23,24 @@ typedef struct {
     int dummy;
 } struct_test2;
 
-template_list3(test, int a, struct_test, struct_test1, struct_test2,
-    int a = 10, b = 11;
-    for (int i = 0;i < a;i++) {
-        printf("a %d\n", i);
+template_list3(int, test, void *d_arg[], struct_test, struct_test1, struct_test2,
+    int a = *(int *)d_arg[1];
+    int b = *(int *)d_arg[0], c = 11;
+    printf("a %d, b %d\n", a, b);
+    for (int i = 0;i < c;i++) {
+        printf("%d\n", i);
     }
-    printf("%ld %s %d\n", sizeof(T), arg->string, a);
+    T in = {.integer = 1};
+    printf("%d\n", in.integer);
+    printf("%ld %s %d %d\n", sizeof(T), arg->string, a, b);
+    return c;
 )
 
 int main(void) {
-    struct_test s = {.integer = 2, .floating = 2, .string = "struct_test"};
-    template_call(struct_test, test, s, s.integer);
+    struct_test s = {.integer = 5, .floating = 2, .string = "struct_test"};
+    struct_test1 s1 = {.integer = 10, .string = "struct_test1"};
+    void *list[2] = {&s.integer, &s1.integer};
+    template_call(struct_test, test, s, list);
+    int get = template_call(struct_test1, test, s1, list);
+    printf("%d\n", get);
 }
